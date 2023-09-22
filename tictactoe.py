34dd5ac5f33e439e2b524edc859318aff09e9868
Daniel Popp <daniel.popp@liebherr.com>
2023-09-22 08:15:41 +0200

    Result and winner

--- tictactoe.py
+++ tictactoe.py
@@ -54,14 +54,36 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+
+    board[action[0]][action[1]] = player(board)
+    return board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    # Check the rows
+    if all(i==board[0][0] for i in board[0]):
+        return board[0][0]
+    elif all(i==board[1][0] for i in board[1]):
+        return board[1][0]
+    elif all(i==board[2][0] for i in board[2]):
+        return board[2][0]
+    # Check columns
+    elif board[0][0] == board[1][0] and board[1][0] == board[2][0]:
+        return board[0][0]
+    elif board[0][1] == board[1][1] and board[1][1] == board[2][1]:
+        return board[0][1]
+    elif board[0][2] == board[1][2] and board[1][2] == board[2][2]:
+        return board[0][2]
+    # Check diagonals
+    elif board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        return board[0][0]
+    elif board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        return board[0][2]
+    else:
+        return None
 
 
 def terminal(board):
